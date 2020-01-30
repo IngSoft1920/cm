d250cc32c5c88a5f970c68acd99d812dbd23b9ce
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-30 17:07:31 +0100

    Setup Spring

---
+++ src/test/java/ingsoft1920/cm/CmApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsoft1920.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
