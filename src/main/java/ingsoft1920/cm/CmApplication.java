eed6a322f1d7ada9b1fd410d8f401d6fc23cae2e
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 15:42:01 +0100

    Incluir Logger + Test ejemplos

--- src/main/java/ingsoft1920/cm/CmApplication.java
+++ src/main/java/ingsoft1920/cm/CmApplication.java
@@ -1,12 +1,16 @@
 package ingsoft1920.cm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CmApplication {
+	final static Logger logger = LogManager.getLogger(CmApplication.class.getName());
 
 	public static void main(String[] args) {
+		logger.warn("Aplicacion iniciada");
 		SpringApplication.run(CmApplication.class, args);
 	}
 
