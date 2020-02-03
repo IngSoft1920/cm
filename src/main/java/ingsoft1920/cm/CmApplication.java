8fc6eae7bdd46b5ce6689a286330bdc82d8eff76
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-02-03 10:36:47 +0100

    Cambio puerto + Controlador prueba

--- src/main/java/ingsoft1920/cm/CmApplication.java
+++ src/main/java/ingsoft1920/cm/CmApplication.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class CmApplication {
@@ -12,6 +14,12 @@ public class CmApplication {
 	public static void main(String[] args) {
 		logger.warn("Aplicacion iniciada");
 		SpringApplication.run(CmApplication.class, args);
+	}
+	
+	@ResponseBody
+	@GetMapping("/helloWorld")
+	public String helloWorldController() {
+		return "Hello world!";
 	}
 
 }
