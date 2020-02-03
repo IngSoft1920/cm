b293bb61d4d7c640e519a8aa897fb6797ab825a7
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-02-03 12:23:06 +0100

    Fix Controller Main

--- src/main/java/ingsoft1920/cm/CmApplication.java
+++ src/main/java/ingsoft1920/cm/CmApplication.java
@@ -4,10 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class CmApplication {
 	final static Logger logger = LogManager.getLogger(CmApplication.class.getName());
 
