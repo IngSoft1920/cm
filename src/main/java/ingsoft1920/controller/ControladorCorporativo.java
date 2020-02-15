f258313a1c585c91f9a3c2f8e1196634b96bed63
Julio <jojurk2010@gmail.com>
2020-02-15 14:55:03 +0100

    Añadida página para añadir banco y funcionalidad de ir y regresar a home

--- src/main/java/ingsoft1920/controller/ControladorCorporativo.java
+++ src/main/java/ingsoft1920/controller/ControladorCorporativo.java
@@ -2,15 +2,32 @@ package ingsoft1920.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ControladorCorporativo {
 
-	@RequestMapping("/home-corp")
+	@GetMapping("/home-corp")
 	public String homeCorporativo() {
 		return "home-corp.jsp";
+	}
+	
+	@GetMapping("/home-corp/anadir-hotel")
+	public String anadirHotelForm() {
+		return "home-corp/anadir-hotel.jsp";
+	}
+	
+	@PostMapping("/home-corp/anadir-hotel")
+	public String anadirHotelTratamientoSolicitud(
+			@RequestParam("nombreHotel") String nombreHotel ,
+			@RequestParam("ubicacionHotel") String ubiHotel) {
+		
+		System.out.printf("\nDatos recibidos correctamente... \n nombre: %s \n ubicación: %s\n",
+							nombreHotel,ubiHotel);
+		return "home-corp/anadir-hotel.jsp";
 	}
 
 }
--- src/main/webapp/WEB-INF/home-corp.jsp
+++ src/main/webapp/WEB-INF/home-corp.jsp
@@ -1,7 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
 
 <html>
+	<!--Esto es para evitar que se subrayen los enlaces-->
+	<body>
+		<style>
+			a { text-decoration: none; }
+		</style
+	</body>
 	
-	<h1>Hola mundo</h1>
+	<h1>Zona corporativa</h1>
+
+	<a href="/home-corp/anadir-hotel"> Añadir hotel </a>
 
 </html>
