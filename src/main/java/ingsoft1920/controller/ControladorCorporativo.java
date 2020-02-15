96eeb7bb3466d737f8f7b073e526786fc2d24b36
Julio <jojurk2010@gmail.com>
2020-02-15 14:09:18 +0100

    Implementada la página base de corporativo

--- src/main/java/ingsoft1920/controller/ControladorCorporativo.java
+++ src/main/java/ingsoft1920/controller/ControladorCorporativo.java
@@ -1,5 +1,16 @@
 package ingsoft1920.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class ControladorCorporativo {
+
+	@RequestMapping("/home-corp")
+	public String homeCorporativo() {
+		return "home-corp.jsp";
+	}
 
 }
--- src/main/java/ingsoft1920/demo/CmApplication.java
+++ src/main/java/ingsoft1920/demo/CmApplication.java
@@ -1,27 +1,18 @@
 package ingsoft1920.demo;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
-@Controller
+// Lo de scanBasePackages es necesario para indicarle a Spring 
+// que busque los controladores en nuestro paquete controller
+@SpringBootApplication(scanBasePackages = {"ingsoft1920.controller"})
 public class CmApplication {
 	final static Logger logger = LogManager.getLogger(CmApplication.class.getName());
 
 	public static void main(String[] args) {
-		logger.warn("Aplicacion iniciada");
+		//logger.warn("Aplicacion iniciada");
 		SpringApplication.run(CmApplication.class, args);
 	}
-	// Soy Jaime
-	@ResponseBody
-	@GetMapping("/helloWorld")
-	public String helloWorldController() {
-		return "Hello world!";
-	}
-
 }
