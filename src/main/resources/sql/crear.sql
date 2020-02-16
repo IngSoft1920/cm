92f72237242ca3d997485e406c0ecdedc8a2e383
Julio <jojurk2010@gmail.com>
2020-02-16 19:36:32 +0100

    Añadidos ficheros sql

---
+++ src/main/resources/sql/crear.sql
@@ -0,0 +1,24 @@
+DROP DATABASE hulio;
+CREATE DATABASE hulio;
+USE hulio;
+
+CREATE TABLE hotel(
+	id INT AUTO_INCREMENT,
+	nombre VARCHAR(40),
+	ubicacion VARCHAR(80),
+	
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE habitacion(
+	id INT AUTO_INCREMENT,
+	precio DECIMAL(4,2), /*Máximo valor posible: 9999.99*/
+	tipo ENUM('normal','premium'),
+	hotel_id INT,
+	
+	PRIMARY KEY(id),
+	FOREIGN KEY (hotel_id) REFERENCES hotel(id) 
+				 		   ON UPDATE RESTRICT
+					 	   ON DELETE CASCADE
+);
+
