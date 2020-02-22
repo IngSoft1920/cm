a762956a0f1ab8edd5e321984e84c2ab4af792c4
Julio <jojurk2010@gmail.com>
2020-02-23 00:50:19 +0100

    Solucionado otro problema de compilación (Por favor, no subáis cosas rotas a develop

--- src/main/java/ingsoft1920/cm/data/ExtractorDatos.java
+++ src/main/java/ingsoft1920/cm/data/ExtractorDatos.java
@@ -2,26 +2,28 @@ package ingsoft1920.cm.data;
 
 import java.util.LinkedList;
 
+import ingsoft1920.model.Peticion;
+
 public class ExtractorDatos {
 
 	public static void main (String[] args) {
-		
+
 		while (true) {
-			
-			LinkedList<Peticion> peticiones = PeticionesDAO.get() //LinkedList de objetos Peticion
-					
+
+			LinkedList<Peticion> peticiones = null; //= PeticionesDAO.get() //LinkedList de objetos Peticion
+
 			for (Peticion pet : peticiones) {
 				if (pet.getEstado() == 0) {
-					
+
 					//UIPath procesa...
-					
-					//UIPath mete en BBDD 
+
+					//UIPath mete en BBDD
 					//o en csv y de ahi a BBDD TODO
-					
-					PeticionesDAO.cambiarEstado(pet.getID());
+
+					//PeticionesDAO.cambiarEstado(pet.getId());
 				}
 			}
 		}
-		
+
 	}
 }
