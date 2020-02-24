0fbaafcfb10fcd9b3dc098ab0ae575748375c4c0
GuilleMunoz <guillemunyoz@gmail.com>
2020-02-24 09:35:47 +0100

    cambios pequeños en precioDAO

--- src/main/java/ingsoft1920/cm/dao/PrecioDAO.java
+++ src/main/java/ingsoft1920/cm/dao/PrecioDAO.java
@@ -17,7 +17,7 @@ public class PrecioDAO {
         if (! conector.isConnected()){
             conector.conectar();
         }
-        String anadirPrecio = "INSERT INTO precio (hotel_id, tipo, fecha, precio) VALUES (?,?,DATE(?),?)";
+        String anadirPrecio = "INSERT INTO precio (hotel_id, tipo, fecha, precio) VALUES (?,?,?,?)";
 
         PreparedStatement stmt;
 
