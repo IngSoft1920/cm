b59b371d1a4487bd060834af209731cd016e9c92
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2020-02-24 18:37:36 +0100

    New Commit: Format ControladorCliente

--- src/main/java/ingsoft1920/cm/controller/ControladorCliente.java
+++ src/main/java/ingsoft1920/cm/controller/ControladorCliente.java
@@ -25,7 +25,7 @@ import ingsoft1920.cm.bean.Precio;
 import ingsoft1920.cm.bean.Reserva;
 
 @Controller
-@SessionAttributes(names = {"cliente","seleccionHabitaciones","fecha_entrada","fecha_salida"})
+@SessionAttributes(names = { "cliente", "seleccionHabitaciones", "fecha_entrada", "fecha_salida" })
 public class ControladorCliente {
 
 	final static Logger logger = LogManager.getLogger(ControladorCliente.class.getName());
@@ -49,12 +49,9 @@ public class ControladorCliente {
 	}
 
 	@PostMapping("/home-client/registro")
-	public String registroForm(Cliente c,Model m) {
+	public String registroForm(Cliente c, Model m) {
 
-		int id = fake.anadirCliente(c.getNombre(),
-						   			c.getDNI(),
-						   			c.getEmail(),
-						   			c.getPassword());
+		int id = fake.anadirCliente(c.getNombre(), c.getDNI(), c.getEmail(), c.getPassword());
 		c.setId(id);
 		m.addAttribute("cliente", c);
 
@@ -67,12 +64,12 @@ public class ControladorCliente {
 	}
 
 	@PostMapping("/home-client/login")
-	public String iniciarSesionForm(String email,String password,Model m) throws Exception {
-		//TODO llamar dao
+	public String iniciarSesionForm(String email, String password, Model m) throws Exception {
+		// TODO llamar dao
 
 		Cliente c = fake.login(email, password);
 
-		if(c==null) {
+		if (c == null) {
 			throw new Exception("Estoy hay que mejorarlo: usuario no existe");
 		}
 
@@ -92,42 +89,34 @@ public class ControladorCliente {
 	}
 
 	@PostMapping("/home-client/main/reservar-buscar")
-	public String reservarForm(String continente,String pais,String ciudad,
-							   Date fecha_entrada,Date fecha_salida,Model m) {
+	public String reservarForm(String continente, String pais, String ciudad, Date fecha_entrada, Date fecha_salida,
+			Model m) {
 
-		int numDias = (int) ((fecha_salida.getTime() - fecha_entrada.getTime())/ 86_400_000);
+		int numDias = (int) ((fecha_salida.getTime() - fecha_entrada.getTime()) / 86_400_000);
 
 		// TODO dao
-		Map<Hotel,List<Precio>> seleccionHabitaciones
-				= fake.habitacionesReserva(continente, pais, ciudad, fecha_entrada, fecha_salida);
+		Map<Hotel, List<Precio>> seleccionHabitaciones = fake.habitacionesReserva(continente, pais, ciudad,
+				fecha_entrada, fecha_salida);
 
-		Map< Hotel , Map<Tipo,Double> > opciones = new HashMap<>();
+		Map<Hotel, Map<Tipo, Double>> opciones = new HashMap<>();
 
-		for( Entry<Hotel,List<Precio>> entrada : seleccionHabitaciones.entrySet() ) {
-			Map<Tipo,Double> preciosNocheHotel = new HashMap<>();
+		for (Entry<Hotel, List<Precio>> entrada : seleccionHabitaciones.entrySet()) {
+			Map<Tipo, Double> preciosNocheHotel = new HashMap<>();
 
-			double precioNormales = entrada
-										.getValue()
-										.stream()
-										.filter(p->p.getTipo()==Tipo.normal)
-										.mapToDouble(p->p.getPrecio())
-										.sum()/numDias;
-			preciosNocheHotel.put(Tipo.normal, Math.floor(precioNormales*100)/100);
+			double precioNormales = entrada.getValue().stream().filter(p -> p.getTipo() == Tipo.normal)
+					.mapToDouble(p -> p.getPrecio()).sum() / numDias;
+			preciosNocheHotel.put(Tipo.normal, Math.floor(precioNormales * 100) / 100);
 
-			double precioPremiums = entrada
-										.getValue()
-										.stream()
-										.filter(p->p.getTipo()==Tipo.premium)
-										.mapToDouble(p->p.getPrecio())
-										.sum()/numDias;
-			preciosNocheHotel.put(Tipo.premium, Math.floor(precioPremiums*100)/100);
+			double precioPremiums = entrada.getValue().stream().filter(p -> p.getTipo() == Tipo.premium)
+					.mapToDouble(p -> p.getPrecio()).sum() / numDias;
+			preciosNocheHotel.put(Tipo.premium, Math.floor(precioPremiums * 100) / 100);
 
 			opciones.put(entrada.getKey(), preciosNocheHotel);
 		}
 
 		m.addAttribute("seleccionHabitaciones", opciones);
-		m.addAttribute("fecha_entrada",fecha_entrada);
-		m.addAttribute("fecha_salida",fecha_salida);
+		m.addAttribute("fecha_entrada", fecha_entrada);
+		m.addAttribute("fecha_salida", fecha_salida);
 		return "redirect:/home-client/main/reservar-resultados";
 	}
 
@@ -138,54 +127,49 @@ public class ControladorCliente {
 
 	@PostMapping("/home-client/main/reservar-resultados")
 	public String recibirReserva(@ModelAttribute("cliente") Cliente clienteSesion,
-								 @ModelAttribute("fecha_entrada") Date fecha_entrada,
-								 @ModelAttribute("fecha_salida") Date fecha_salida,
-								 int hotel_id, double importe, Tipo tipo) {
+			@ModelAttribute("fecha_entrada") Date fecha_entrada, @ModelAttribute("fecha_salida") Date fecha_salida,
+			int hotel_id, double importe, Tipo tipo) {
 
-		//TODO dao
+		// TODO dao
 		fake.anadirReserva(fecha_entrada, fecha_salida, importe, hotel_id, tipo, clienteSesion.getId());
 
 		return "redirect:/home-client/main";
 	}
 
-
 	@GetMapping("/home-client/main/cancelar-reserva")
-	public String cancelarReserva(@ModelAttribute("cliente") Cliente clienteSesion,
-								  Model m) {
+	public String cancelarReserva(@ModelAttribute("cliente") Cliente clienteSesion, Model m) {
 
-		//TODO dao
-		Map<Reserva,Hotel> reservasCliente = fake.reservasCliente(clienteSesion.getId());
-		m.addAttribute("reservasMap",reservasCliente);
+		// TODO dao
+		Map<Reserva, Hotel> reservasCliente = fake.reservasCliente(clienteSesion.getId());
+		m.addAttribute("reservasMap", reservasCliente);
 
 		return "/home-client/main/cancelar-reserva.jsp";
 	}
 
 	@PostMapping("/home-client/main/cancelar-reserva")
 	public String recibirCancelacionReserva(int reserva_id) {
-		System.out.println("recibido "+reserva_id);
+		System.out.println("recibido " + reserva_id);
 
-		//TODO dao
+		// TODO dao
 		fake.cancelarReserva(reserva_id);
 		return "redirect:/home-client/main";
 	}
 
 	@GetMapping("/home-client/main/visualizar-reservas")
-	public String visualizarReservas(@ModelAttribute("cliente") Cliente clienteSesion,
-									 Model m) {
+	public String visualizarReservas(@ModelAttribute("cliente") Cliente clienteSesion, Model m) {
 
 		// TODO llamar dao
-		Map<Reserva,Hotel> reservasCliente = fake.reservasCliente(clienteSesion.getId());
-		m.addAttribute("reservasMap",reservasCliente);
+		Map<Reserva, Hotel> reservasCliente = fake.reservasCliente(clienteSesion.getId());
+		m.addAttribute("reservasMap", reservasCliente);
 
 		return "/home-client/main/visualizar-reservas.jsp";
 	}
 
 	@GetMapping("/home-client/main/visualizar-facturas")
-	public String visualizarFacturas(@ModelAttribute("cliente") Cliente clienteSesion,
-									 Model m) {
+	public String visualizarFacturas(@ModelAttribute("cliente") Cliente clienteSesion, Model m) {
 
 		// TODO llamar dao
-		List<Factura> facturasCliente = fake.facturasCliente( clienteSesion.getId() );
+		List<Factura> facturasCliente = fake.facturasCliente(clienteSesion.getId());
 		m.addAttribute("facturas", facturasCliente);
 
 		return "/home-client/main/visualizar-facturas.jsp";
@@ -193,7 +177,7 @@ public class ControladorCliente {
 
 	@GetMapping("/home-client/main/feedback")
 	public String realizarValoracion(Model m) {
-		//TODO llamar dao
+		// TODO llamar dao
 
 		List<Hotel> hoteles = fake.hoteles();
 		m.addAttribute("hoteles", hoteles);
@@ -207,29 +191,13 @@ public class ControladorCliente {
 	}
 
 	@PostMapping("/home-client/main/feedback-escribir/{hotel_id}")
-	public String recibirValoracion(@PathVariable int hotel_id,
-									@ModelAttribute("cliente") Cliente c,
-									String cabecera,String cuerpo,double nota) {
+	public String recibirValoracion(@PathVariable int hotel_id, @ModelAttribute("cliente") Cliente c, String cabecera,
+			String cuerpo, double nota) {
 
 		fake.anadirValoracion(cabecera, cuerpo, nota, c.getId(), hotel_id);
 
-		System.out.println( fake.feedback() );
+		System.out.println(fake.feedback());
 		return "redirect:/home-client/main";
 	}
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
