1495b3bd70dd6cff56778df7f2b6b92b53816b08
ijencinas <ij.encinas@alumnos.upm.es>
2020-02-24 19:22:20 +0100

    Queries para RM en PrecioDAO y PeticionDAO

--- src/main/java/ingsoft1920/cm/dao/PeticionesDAO.java
+++ src/main/java/ingsoft1920/cm/dao/PeticionesDAO.java
@@ -1,5 +1,6 @@
 package ingsoft1920.cm.dao;
 
+import ingsoft1920.cm.bean.Habitaciones.Tipo;
 import ingsoft1920.cm.conector.conectorBBDD;
 import ingsoft1920.cm.model.Peticion;
 
@@ -11,9 +12,9 @@ import java.util.LinkedList;
 
 public class PeticionesDAO {
 
-    private static conectorBBDD conector = new conectorBBDD("8000", "cm1", "ingSoft20ge1.711", "piedrafita.ls.fi.upm.es");
+    private static conectorBBDD conector = new conectorBBDD("8000", "cm1", "ingSoft20cm1.711", "piedrafita.ls.fi.upm.es");
 
-    private LinkedList<Peticion> getPeticiones(int id) {
+    public LinkedList<Peticion> getPeticiones(int id) {
 
         if (!conector.isConnected()){
             conector.conectar();
@@ -24,14 +25,14 @@ public class PeticionesDAO {
         PreparedStatement stmt = null;
         ResultSet rs = null;
 
-        String getPeticiones = "SELECT * FROM peticion";
+        String getPeticiones = "SELECT * FROM peticiones";
 
         try {
             stmt = conector.getConn().prepareStatement(getPeticiones);
             rs = stmt.executeQuery();
 
             while (rs.next()){
-                peticiones.add(new Peticion(rs.getInt("id"), rs.getString("ciudad"), rs.getString("fecha"), rs.getDate("tipo").toString(), rs.getInt("estado")));
+                peticiones.add(new Peticion(rs.getInt("id"), rs.getString("ciudad"), rs.getString("fecha_peticiones"), rs.getDate("tipo_habitacion").toString(), rs.getInt("estado")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -46,12 +47,13 @@ public class PeticionesDAO {
             conector.conectar();
         }
 
-        String cambiaEstado = "UPDATE peticion SET estado = 1 WHERE id = ?";
+        String cambiaEstado = "UPDATE peticiones SET estado = 1 WHERE id = ?;";
 
         PreparedStatement stmt = null;
 
         try {
             stmt = conector.getConn().prepareStatement(cambiaEstado);
+            stmt.setInt(1, id);
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -59,4 +61,45 @@ public class PeticionesDAO {
 
         conector.closeConn();
     }
+    
+    
+    public int addPeticion(String ciudad, Date fecha, Tipo tipo_habitacion,int estado) {
+    		PreparedStatement st=null;
+    		PreparedStatement st0=null;
+		ResultSet rs=null;  
+		int id=-1;
+		  if (! conector.isConnected())
+	          conector.conectar();
+	      
+		  try {
+			  st=conector.getConn().prepareStatement("Add into peticiones(ciudad, fecha_peticiones, tipo_habitacion, estado) "
+			  		+ "values(?,?,?,?);");
+			  st.setString(1, ciudad);	
+			  st.setDate(2,fecha);
+			  st.setString(3, tipo_habitacion.toString());
+			  st.setInt(4, estado);
+			  st.executeQuery();
+			  
+			  st0=conector.getConn().prepareStatement("Select max(id) from peticiones");
+			  rs=st.executeQuery();
+			  id=rs.getInt("id");
+			  
+		  } catch (SQLException e) {
+				e.printStackTrace();
+			}finally { 
+				if (rs != null) { try { rs.close(); } catch (SQLException sqlEx) { } rs = null; } 
+				if (st != null) { try { st.close(); } catch (SQLException sqlEx) { }  st = null; } 
+				if (st0 != null) { try { st0.close(); } catch (SQLException sqlEx) { }  st0 = null; } 
+				conector.closeConn();
+			}
+    	
+    		return id;
+    }
+    
+    
+		  
 }
+    
+    
+
+
--- src/main/java/ingsoft1920/cm/dao/PrecioDAO.java
+++ src/main/java/ingsoft1920/cm/dao/PrecioDAO.java
@@ -1,5 +1,6 @@
 package ingsoft1920.cm.dao;
 
+import ingsoft1920.cm.bean.Habitaciones.Tipo;
 import ingsoft1920.cm.conector.conectorBBDD;
 import ingsoft1920.cm.model.Precio;
 
@@ -9,7 +10,7 @@ import java.sql.SQLException;
 
 public class PrecioDAO {
 
-    private static conectorBBDD conector = new conectorBBDD("8000", "cm1", "ingSoft20ge1.711", "piedrafita.ls.fi.upm.es");
+    private static conectorBBDD conector = new conectorBBDD("8000", "cm1", "ingSoft20cm1.711", "piedrafita.ls.fi.upm.es");
 
     //Recibe un objeto Precio (definido en ingsof1920/model)
     public void anadirPrecio(Precio precio){
@@ -36,5 +37,54 @@ public class PrecioDAO {
         conector.closeConn();
 
     }
+    
+    public void setPrecio(int id_hotel, Tipo tipoHabitacion,Date fecha, float precio) {
+		  PreparedStatement st=null;
+		  
+		  if (! conector.isConnected())
+	          conector.conectar();
+	      
+		  try {
+			  st=conector.getConn().prepareStatement
+					  ("Add into precio(id_hotel, tipo_habitacion, fecha, valor) values(?, ?, ?, ?);");
+			  
+			  st.setInt(1, id_hotel);	
+			  st.setString(2,tipoHabitacion.toString());
+			  st.setDate(3, fecha);
+			  st.setFloat(4,precio);
+			  st.executeQuery();
+			  
+		  } catch (SQLException e) {
+				e.printStackTrace();
+			}finally { 
 
+				if (st != null) { try { st.close(); } catch (SQLException sqlEx) { }  st = null; }
+				conector.closeConn();
+			}
+	  }
+    
+    public void addDatosPrecio(int idPeticion, float precio, int puntuacion) {
+		  PreparedStatement st=null;
+		  
+		  if (! conector.isConnected())
+	          conector.conectar();
+	      
+		  try {
+			  st=conector.getConn().prepareStatement("Add into datos_precios(id_peticion, precio,puntuacion) "
+			  		+ "values(?,?,?);");
+			  st.setInt(1, idPeticion);	
+			  st.setFloat(2,precio);
+			  st.setInt(3, puntuacion);
+			  st.executeQuery();
+			  
+		  } catch (SQLException e) {
+				e.printStackTrace();
+			}finally { 
+
+				if (st != null) { try { st.close(); } catch (SQLException sqlEx) { }  st = null; }
+				conector.closeConn();
+			}
+
+    }
+    
 }
