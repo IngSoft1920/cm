32eadb52c48d90038030d92242c083905eb84493
Jacobo <jacobo.matap@alumnos.upm.es>
2020-03-09 11:11:44 +0100

    Añadidos DAO Servicio y Profesión
    Las funciones añadir y listar

---
+++ src/main/java/ingsoft1920/cm/dao/ProfesionDAO.java
@@ -0,0 +1,58 @@
+package ingsoft1920.cm.dao;
+
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ingsoft1920.cm.bean.Profesion;
+import ingsoft1920.cm.conector.ConectorBBDD;
+
+@Component
+public class ProfesionDAO {
+
+	@Autowired
+	private QueryRunner runner = new QueryRunner();
+
+	@Autowired
+	private ConectorBBDD conector = new ConectorBBDD();
+
+
+	
+	public int anadir(Profesion s) {
+		// Primero añadimos el hotel mismamente
+		BigInteger res = null;
+		ScalarHandler<BigInteger> handler = new ScalarHandler<>();
+		String queryH = "INSERT INTO Profesion " + "(nombre) "
+				+ "VALUES (?);";
+
+		try (Connection conn = conector.getConn()) {
+			res = runner.insert(conn, queryH, handler, s.getNombre());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return (res != null ? res.intValue() : -1);
+	}
+
+	public List<Profesion> Profesions() {
+		List<Profesion> res = new ArrayList<>();
+		BeanListHandler<Profesion> handler = new BeanListHandler<>(Profesion.class);
+		String query = "SELECT * FROM Servcio";
+
+		try (Connection conn = conector.getConn()) {
+			res = runner.query(conn, query, handler);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+}
