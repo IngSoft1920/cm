845c83fb7e7a94b52a6d53ad37b30787289206ba
ijencinas <ij.encinas@alumnos.upm.es>
2020-03-13 14:02:26 +0100

    Cambios controller/vista

--- src/main/java/ingsoft1920/cm/controller/BeneficioController.java
+++ src/main/java/ingsoft1920/cm/controller/BeneficioController.java
@@ -1,20 +1,29 @@
 package ingsoft1920.cm.controller;
 
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ingsoft1920.cm.fna.FacturaDAO;
+import ingsoft1920.cm.fna.BeneficiosGastosModel;
+import ingsoft1920.cm.fna.ConexionEM;
+
 @Controller
 public class BeneficioController {
 	
-	private FacturaDAO beneficio=new FacturaDAO();
-	private double ganancias=0;
+	private HashMap <Integer, BeneficiosGastosModel> beneficios_gastos=FacturaDAO.beneficioPorHotel();
+	
 	@GetMapping("/beneficio")
-	public String beneficio() {
+	public String beneficio(Model model) {
+		beneficios_gastos=FacturaDAO.gastosAlimentosPorHotel(beneficios_gastos);
+		beneficios_gastos=ConexionEM.peticionSueldoEmpleados(beneficios_gastos);
+		model.addAttribute("treasureMap", beneficios_gastos);
 		return "fna/beneficio.jsp";
 	}
 	
+	/*
 	@PostMapping("/beneficio")
 	public String beneficioHotel(@RequestParam("hotel_id") int hotel_id) {
 		
@@ -30,6 +39,6 @@ public class BeneficioController {
 		return "fna/beneficio-result.jsp";
 	}
 	
-	
+	*/
 
 }
--- src/main/webapp/WEB-INF/fna/beneficio.jsp
+++ src/main/webapp/WEB-INF/fna/beneficio.jsp
@@ -11,12 +11,15 @@
 	<body>
 		<h1>Beneficios</h1>
 
-		
-			<form method="POST">
-				Hotel ID: <input type="integer" name="hotel_id" >
-                <input type="submit" value="Submit" />
-            </form>
-		
+			<c:forEach items="${treasureMap}" var="entry">
+				   
+				   <b>Hotel:</b> ${entry.value.nombreHotel}$  <b>ID:</b> ${entry.key}$<br>
+				   <b> Dinero de las reservas:</b> ${entry.value.sumaReservas}$<br>
+				   <b> Suma de las facturas:</b> ${entry.value.sumaFacturas}$<br>
+				   <b> Dinero Invertido en Empleados:</b> ${entry.value.sueldoEmpleados}$<br>
+				   <b> Dinero Invertido en Comida:</b> ${entry.value.gastoComida}$<b
+
+			</c:forEach>
 	</body>
 
 </html>
