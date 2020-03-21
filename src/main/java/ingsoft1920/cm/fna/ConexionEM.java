96748f31c4915ad0d691d8b370c42ca4a11692f0
Sergio Carulli <sergiomario.carulli.perez@alumnos.upm.es>
2020-03-21 18:38:19 +0100

    Desglosar dineroEmpleados por rol

--- src/main/java/ingsoft1920/cm/fna/ConexionEM.java
+++ src/main/java/ingsoft1920/cm/fna/ConexionEM.java
@@ -1,11 +1,6 @@
 package ingsoft1920.cm.fna;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
-
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -22,25 +17,46 @@ public class ConexionEM {
 				String resp=client.getResponseBody();
 				JsonObject obj = (JsonObject) JsonParser.parseString(resp);
 				//obtenemos los campos
-				JsonArray id_HotelLista=obj.get("id_hotel").getAsJsonArray();
+				JsonArray id_HotelLista=obj.get("hotel_id").getAsJsonArray();
 				int[] id_hotel= new int[id_HotelLista.size()];
-				JsonArray sueldoLista=obj.get("valor").getAsJsonArray();
+				JsonArray sueldoLista=obj.get("sueldo").getAsJsonArray();
 				double[] sueldo= new double[sueldoLista.size()];
+				JsonArray id_empleadoLista=obj.get("empleado_id").getAsJsonArray();
+				int[] id_empleado= new int[id_empleadoLista.size()];
+				JsonArray rolLista=obj.get("rol").getAsJsonArray();
+				String[] rol = new String[rolLista.size()];
+				JsonArray incentivoLista=obj.get("incentivo").getAsJsonArray();
+				double[] incentivo= new double[incentivoLista.size()];
 				//Creamos un objeto auxiliar
 				BeneficiosGastosModel aux;
+				Double sueldoEmpleados;
 				//Añadimos la info
 				for(int i=0;i<id_HotelLista.size();i++) {
 					id_hotel[i]=id_HotelLista.get(i).getAsInt();
 					sueldo[i]=sueldoLista.get(i).getAsDouble();
+					id_empleado[i]=id_HotelLista.get(i).getAsInt();
+					rol[i]=sueldoLista.get(i).getAsString();
+					incentivo[i]=id_HotelLista.get(i).getAsDouble();
 					aux=map.get(id_hotel[i]);
-					System.out.println("Hotel_id: "+id_hotel[i]+" , sueldo: "+sueldo[i]);
+					System.out.println("Hotel_id: "+id_hotel[i]+" , sueldo: "+sueldo[i]+", id_empleado: "+id_empleado[i]+", rol: "+rol[i]+", incentivo: "+incentivo[i]);
 					if(aux!=null) {
-						//Añadimos el gasto en pagar empleados
-						aux.setSueldoEmpleados(sueldo[i]);
+						sueldoEmpleados = aux.getSueldoEmpleados().get(rol[i]);
+						if(sueldoEmpleados!=null) {
+							//Ya hay una entrada creada para ese rol. Actualizar el value
+							aux.getSueldoEmpleados().replace(rol[i], sueldoEmpleados+sueldo[i]+incentivo[i]);
+						}
+						else {
+							//No hay una entrada creada, la creamos y añadimos los valores.
+							aux.getSueldoEmpleados().put(rol[i], sueldo[i]+incentivo[i]);
+						}
 					}
 					else {
 						//Si se mete aqui, deberia haber algun error de sincronizacion entre las distintas aplicaciones. No veo 
-						//factible que haya un hotel cuyo unico movimiento monetario sea pagar a los empleados
+						//factible que haya un hotel cuyo unico movimiento monetario sea pagar a los empleados.
+						//Aun asi, por si acaso, creamos una nueva entrada en el map
+						aux=new BeneficiosGastosModel("",0,0,0);
+						aux.getSueldoEmpleados().put(rol[i], sueldo[i]+incentivo[i]);
+						map.put(id_hotel[i], aux);
 					}
 				}
 			}
