c2cb4d21e3ce241098c204b3b8d0ba6aff48f5d7
Julio Castro <jojurk2010@gmail.com>
2020-03-29 03:30:04 +0200

    Refactor ServicioDAO

--- src/main/java/ingsoft1920/cm/dao/ServicioDAO.java
+++ src/main/java/ingsoft1920/cm/dao/ServicioDAO.java
@@ -1,9 +1,10 @@
 package ingsoft1920.cm.dao;
 
-import java.math.BigInteger;
+import java.math.BigInteger;	
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
@@ -12,9 +13,7 @@ import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import ingsoft1920.cm.bean.Categoria;
 import ingsoft1920.cm.bean.Servicio;
-import ingsoft1920.cm.bean.auxiliares.Servicio_Profesion;
 import ingsoft1920.cm.conector.ConectorBBDD;
 
 @Component
@@ -26,42 +25,48 @@ public class ServicioDAO {
 	@Autowired
 	private ConectorBBDD conector = new ConectorBBDD();
 
-	public int anadir(Servicio s, List<Servicio_Profesion> profesiones) {
+	// Cada Properties nos da info de las profesiones que atienden
+	// a dicho servicio:
+	// -profesion_id: int
+	public int anadir(Servicio s, List<Properties> info) {
 		BigInteger res = null;
 		ScalarHandler<BigInteger> handler = new ScalarHandler<>();
 
-		String queryS = "INSERT INTO Servicio " + "(nombre) " + "VALUES (?);";
+		String queryServ = "INSERT INTO Servicio "
+						  +"(nombre) "
+						  +"VALUES (?);";
 
-		String queryPro = "INSERT INTO Servicio_Profesion " + "(profesion_id,servicio_id) " + "VALUES (?,?)";
+		String queryProfs = "INSERT INTO Servicio_Profesion "
+						   +"(servicio_id,profesion_id) "
+						   +"VALUES (?,?)";
 
 		List<Object[]> batch;
 		try (Connection conn = conector.getConn()) {
-			res = runner.insert(conn, queryS, handler, s.getNombre());
+			res = runner.insert(conn, queryServ, handler, s.getNombre());
 	
 			batch = new ArrayList<>();
-			for (Servicio_Profesion pro : profesiones) {
-				batch.add(new Object[] { res.intValue(), pro.getProfesion_id() });
+			for (Properties prof : info) {
+				batch.add(new Object[] { res.intValue(),
+										 prof.get("profesion_id") 
+									   });
 			}
-			runner.batch(conn, queryPro, batch.toArray(new Object[profesiones.size()][]));
+			runner.batch(conn, queryProfs, batch.toArray(new Object[info.size()][]));
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) { e.printStackTrace(); }
 
 		return (res != null ? res.intValue() : -1);
 	}
 	
-	public Servicio servicio(int id) {
+	public Servicio getByID(int servicioID) {
 		Servicio res=null;
 		BeanHandler<Servicio> handler = new BeanHandler<>(Servicio.class);
-		String query = "SELECT * FROM Servicio WHERE id = "+ id;
+		String query = "SELECT * FROM Servicio WHERE id = ?";
 
 		try (Connection conn = conector.getConn()) {
-			res = runner.query(conn, query, handler);
+			res = runner.query(conn, query, handler,servicioID);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) { e.printStackTrace(); }
+		
 		return res;
 	}
 
@@ -73,28 +78,9 @@ public class ServicioDAO {
 		try (Connection conn = conector.getConn()) {
 			res = runner.query(conn, query, handler);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) { e.printStackTrace(); }
+		
 		return res;
-	}
-	
-	public Servicio get(int id){
-
-        String selectQuery = "SELECT * FROM Servicio WHERE id = ?";
-
-        BeanHandler<Servicio> handler = new BeanHandler<>(Servicio.class);
-
-        Servicio res = null;
-
-        try ( Connection conn = conector.getConn() )
-        {
-            res = runner.query(conn, selectQuery, handler, id);
-
-        } catch(Exception e) { e.printStackTrace(); }
-
-        return res;
-    }
-	
+	}	
 
 }
