9432ce50d9ee52f8ae39318342a18894fde645f3
Julio Castro <jojurk2010@gmail.com>
2020-05-23 00:21:01 +0200

    Corregidas inconsistencias de beans

--- src/main/java/ingsoft1920/cm/bean/Pedido.java
+++ src/main/java/ingsoft1920/cm/bean/Pedido.java
@@ -6,14 +6,19 @@ public class Pedido {
 	
 	private int id;
 	private Date fecha;
+	private int proveedor_id;
 	private int hotel_id;
 	
 	public Pedido() {}
-	public Pedido(int id, Date fecha, int hotel_id) {
+	
+	public Pedido(int id, Date fecha, int proveedor_id, int hotel_id) {
+		super();
 		this.id = id;
 		this.fecha = fecha;
+		this.proveedor_id = proveedor_id;
 		this.hotel_id = hotel_id;
 	}
+
 	public int getId() {
 		return id;
 	}
@@ -26,17 +31,23 @@ public class Pedido {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+	public int getProveedor_id() {
+		return proveedor_id;
+	}
+	public void setProveedor_id(int proveedor_id) {
+		this.proveedor_id = proveedor_id;
+	}
 	public int getHotel_id() {
 		return hotel_id;
 	}
 	public void setHotel_id(int hotel_id) {
 		this.hotel_id = hotel_id;
 	}
+
 	@Override
 	public String toString() {
-		return "Pedido [id=" + id + ", fecha=" + fecha + ", hotel_id=" + hotel_id + "]";
+		return "Pedido [id=" + id + ", fecha=" + fecha + ", proveedor_id=" + proveedor_id + ", hotel_id=" + hotel_id
+				+ "]";
 	}
-	
-	
 
 }
--- src/main/java/ingsoft1920/cm/dao/ProductoDAO.java
+++ src/main/java/ingsoft1920/cm/dao/ProductoDAO.java
@@ -1,6 +1,6 @@
 package ingsoft1920.cm.dao;
 
-import java.math.BigInteger;
+import java.math.BigInteger;	
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ingsoft1920.cm.bean.Producto;
-import ingsoft1920.cm.bean.Proveedor_Producto;
 import ingsoft1920.cm.conector.ConectorBBDD;
 
 @Component
@@ -86,27 +85,20 @@ public class ProductoDAO {
 		return res;
 	}
 	
-	public Proveedor_Producto infoproducto(int producto_id, int proveedor_id) {
-		Proveedor_Producto res=null;
-		BeanHandler<Proveedor_Producto> handler = new BeanHandler<>(Proveedor_Producto.class);
-		String query = "SELECT * "
-					  +"FROM Proveedor_Producto "
-					  +"WHERE producto_id=? AND proveedor_id=? ";
-
-		try (Connection conn = conector.getConn()) 
-		{
-			res = runner.query(conn, query, handler,producto_id, proveedor_id);
-
-		} catch (Exception e) { e.printStackTrace(); }
-		
-		return res;
-	}
-
-	
-	public static void main(String[] args) {
-		Producto prod = new Producto(-1, "Bananas");
-		new ProductoDAO().anadir(prod);
-	}
-	
+//	public Proveedor_Producto infoproducto(int producto_id, int proveedor_id) {
+//		Proveedor_Producto res=null;
+//		BeanHandler<Proveedor_Producto> handler = new BeanHandler<>(Proveedor_Producto.class);
+//		String query = "SELECT * "
+//					  +"FROM Proveedor_Producto "
+//					  +"WHERE producto_id=? AND proveedor_id=? ";
+//
+//		try (Connection conn = conector.getConn()) 
+//		{
+//			res = runner.query(conn, query, handler,producto_id, proveedor_id);
+//
+//		} catch (Exception e) { e.printStackTrace(); }
+//		
+//		return res;
+//	}	
 
 }
